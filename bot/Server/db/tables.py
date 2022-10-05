2bb8c034ba7531ddabb0d6fa2e243c8418a06a30
NIDILLIN <writebutread@outlook.com>
2022-10-05 21:21:49 +0300

    .

--- bot/Server/db/tables.py
+++ bot/Server/db/tables.py
@@ -51,19 +51,13 @@ class Tokens(Table):
     class Column():
         token = 'token'
         short_name = 'short_name'
-        current = 'current'
+        current = 0
 
 
     async def insert(self):
-        try:
-            await self.db.execute(
-                f'DELETE FROM tokens token where short_name={self.short_name}'
-            )
-        except:
-            pass
         await self.db.execute(
-            f'INSERT INTO tokens VALUES (?, ?)', 
-            (self.token, self.short_name)
+            f'INSERT INTO tokens VALUES (?, ?, ?)', 
+            (self.token, self.short_name, self.current)
         )
         await self.db.commit()
 
@@ -86,23 +80,32 @@ class Tokens(Table):
             f'SELECT {Tokens.Column.token} FROM tokens WHERE short_name=?', (value, )
         )
         token = await cursor.fetchone()
-        return token[0]
+        return token[0] if token else None
 
     async def get_current_acc(self) -> Optional[str]:
         cursor = await self.db.execute(
             f'SELECT short_name FROM tokens WHERE current=?', (1, )
         )
         name = await cursor.fetchone()
-        return name[0]
+        return name[0] if name else None
+
+    async def get_current_token(self) -> Optional[str]:
+        cursor = await self.db.execute(
+            f'SELECT token FROM tokens WHERE current=?', (1, )
+        )
+        token = await cursor.fetchone()
+        return token[0] if token else None
 
     async def set_current_acc(self, short_name: str):
+        current = await self.get_current_acc()
         await self.db.execute(
-            f'UPDATE tokens SET current=0'
+            f'UPDATE tokens SET current=0 WHERE short_name=?', (current,)
         )
         await self.db.execute(
-            f'UPDATE tokens SET current=1 where short_name={short_name}'
+            f'UPDATE tokens SET current=1 WHERE short_name=?', (short_name, )
         )
         await self.db.commit()
+
 
 class Notes(Table):
     note: str = 'none'
