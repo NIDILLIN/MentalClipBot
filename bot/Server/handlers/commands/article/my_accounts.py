846b77f56b5f91a1a24e4a72ef5e678d793789ba
NIDILLIN <writebutread@outlook.com>
2022-10-05 21:20:57 +0300

    current account and switching to another

--- bot/Server/handlers/commands/article/my_accounts.py
+++ bot/Server/handlers/commands/article/my_accounts.py
@@ -48,8 +48,12 @@ async def my_accounts(call: types.CallbackQuery):
             callback_data=acc.new(short_name=name)
         ) for name in accounts]
     keyboard = types.InlineKeyboardMarkup(row_width=1).add(*buttons)
+    current_acc = await user.tokens.get_current_acc()
+    if not current_acc:
+        current_acc = 'не установлен'
     await call.message.edit_text(
-        'Мои аккаунты',
+        'Мои аккаунты\n'+
+        f'Текущий: {current_acc}',
         reply_markup=keyboard
     )
 
@@ -58,13 +62,14 @@ async def my_account(call: types.CallbackQuery, callback_data: dict):
     short_name = callback_data['short_name']
     user = await UserDB(call.from_user.id).connect()
     token = await user.tokens.get_by_short_name(short_name)
-    pages = await telegraph_requests.get_pages(token)
+    pages = await telegraph_requests.get_pages_count(token)
     auth_url = await telegraph_requests.get_auth_url(token)
 
     text = f'<b>Аккаунт:</b> {short_name}\n<b>Кол-во статей аккаунта:</b> {pages}'
-    log_in = types.InlineKeyboardButton('Войти на этом девайсе', url=auth_url, callback_data=log_acc.new(short_name=short_name))
+    log_in = types.InlineKeyboardButton('Войти на этом девайсе', url=auth_url)
+    change_to = types.InlineKeyboardButton('Пееключиться на этот аккаунт', callback_data=log_acc.new(short_name=short_name))
     back = types.InlineKeyboardButton('Назад', callback_data='back_accounts')
-    keyboard = types.InlineKeyboardMarkup(row_width=1).add(log_in, back)
+    keyboard = types.InlineKeyboardMarkup(row_width=1).add(log_in, change_to, back)
 
     await call.message.edit_text(
         text=text,
@@ -76,6 +81,7 @@ async def my_account(call: types.CallbackQuery, callback_data: dict):
 async def set_current_acc(call: types.CallbackQuery, callback_data: dict):
     user = await UserDB(call.from_user.id).connect()
     await user.tokens.set_current_acc(callback_data['short_name'])
+    await call.answer('Вы переключились на этот аккаунт', show_alert=True)
 
 
 def register_my_accounts(dp: Dispatcher):
