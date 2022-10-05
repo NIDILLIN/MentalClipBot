d4029af783f8c017c6e719bf9b7f1c0bd6857e3a
NIDILLIN <writebutread@outlook.com>
2022-10-05 21:22:07 +0300

    Articles list and article menu

--- bot/Server/handlers/commands/article/article.py
+++ bot/Server/handlers/commands/article/article.py
@@ -1,54 +1,113 @@
-import aiohttp
 from aiogram import Dispatcher
 from aiogram import types
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
-from telegraph.aio import Telegraph
+from aiogram.utils.callback_data import CallbackData
+from aiogram.dispatcher.filters import Text
+import Server.handlers.commands.article.telegraph_requests as telegraph_requests
 
 from Server.db.db import UserDB
 
+cb_article = CallbackData('article', 'path')
+
+class Article():
+    title: str
+    description: str
+    url: str
 
 
-article = State()
+    def __init__(self, title, description, url) -> None:
+        self.title = title
+        self.description = description
+        self.url = url
+
+    async def text(self):
+        t = (
+            '<b>Заголовок:</b> '
+            +f"""<a href="{self.url}">{self.title}</a>"""
+            +'\n\n'
+            +'<b>Описание:</b>\n'
+            +f'{self.description}'
+        )
+        return t
 
 
 async def cmd_new_article(message: types.Message) -> None:
     to_telegraph = types.InlineKeyboardButton(text='Перейти в telegraph', url='https://telegra.ph/')
     keyboard = types.InlineKeyboardMarkup().add(to_telegraph)
     await message.answer(
-        'Напиши статью',
+        'Создать статью',
         disable_web_page_preview=True,
         protect_content=True,
         reply_markup=keyboard
     )
 
-async def my_articles(message: types.Message):
-    articles_list = types.InlineKeyboardMarkup()
-    article = types.InlineKeyboardButton(
-        'title',
-        url=article_url
-    )
+async def cmd_my_articles(message: types.Message):
     user = await UserDB(message.from_user.id).connect()
-    data={'access_token': user.token}
-    await get_pages(data)
+    token = await user.tokens.get_current_token()
+    account = await user.tokens.get_current_acc()
+    pages = await telegraph_requests.get_pages_count(token)
+    if pages == 0:
+        await message.answer(f'Нет статей для текущего аккаунта ({account})')
+        return
+    pages_list = await telegraph_requests.get_pages_list(token)
+    articles_list = types.InlineKeyboardMarkup()
+    buttons = []
+    for page in pages_list:
+        buttons.append(
+            types.InlineKeyboardButton(
+                text=page['title'],
+                callback_data=cb_article.new(path=page['path'])
+            )
+        )
+    articles_list.add(*buttons)
+    await message.answer(
+        f'Статьи для текущего аккаунта ({account})',
+        reply_markup=articles_list
+    )
 
 
-async def get_pages(data):
-    async with aiohttp.ClientSession() as session:
-        url = "https://api.telegra.ph/getPageList?"
-        async with session.get(url, params=data) as resp:
-            result = await resp.json()
-    return result
+async def articles(call: types.CallbackQuery):
+    user = await UserDB(call.from_user.id).connect()
+    token = await user.tokens.get_current_token()
+    pages = await telegraph_requests.get_pages_count(token)
+    if pages == 0:
+        await call.message.answer(f'Нет статей для текущего аккаунта ({account})')
+        return
+    account = await user.tokens.get_current_acc()
+    pages_list = await telegraph_requests.get_pages_list(token)
+    articles_list = types.InlineKeyboardMarkup()
+    buttons = []
+    for page in pages_list:
+        buttons.append(
+            types.InlineKeyboardButton(
+                text=page['title'],
+                callback_data=cb_article.new(path=page['path'])
+            )
+        )
+    articles_list.add(*buttons)
+    await call.message.edit_text(
+        f'Статьи для текущего аккаунта ({account})',
+        reply_markup=articles_list
+    )
 
 
-async def get_auth_url(token):
-    async with aiohttp.ClientSession() as session:
-        url = f"""https://api.telegra.ph/getAccountInfo?access_token={token}&fields=["auth_url"]"""
-        async with session.get(url) as resp:
-            result = await resp.json()
-    return result['result']['auth_url']
+async def article(call: types.CallbackQuery, callback_data: dict):
+    page_info = await telegraph_requests.get_page_info(callback_data['path'])
+    text = await Article(
+        title=page_info['title'],
+        description=page_info['description'],
+        url=page_info['url']
+    ).text()
+    back = types.InlineKeyboardButton('Назад', callback_data='back_articles')
+    keyboard = types.InlineKeyboardMarkup(row_width=1).add(back)
+    await call.message.edit_text(
+        text=text,
+        reply_markup=keyboard
+    )
 
 
 def register_article_creating(dp: Dispatcher) -> None:
     dp.register_message_handler(cmd_new_article, commands='new_article', state='*')
-    # dp.register_message_handler(cmd_new_article, commands='my_articles', state='*')
+    dp.register_message_handler(cmd_my_articles, commands='my_articles', state='*')
+
+    dp.register_callback_query_handler(article, cb_article.filter())
+    dp.register_callback_query_handler(articles, Text('back_articles'))
