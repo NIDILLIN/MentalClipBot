c8ea484cab39d868806ddaf63c6b4df51cbf6d63
NIDILLIN <writebutread@outlook.com>
2022-10-06 10:30:06 +0300

    CMD profile

---
+++ bot/Server/handlers/commands/article/profile/my_profile.py
@@ -0,0 +1,27 @@
+from aiogram import Dispatcher
+from aiogram import types
+from aiogram.utils.callback_data import CallbackData
+from aiogram.dispatcher.filters import Text
+import Server.handlers.commands.article.telegraph_requests as telegraph_requests
+
+from Server.db.db import UserDB
+
+
+async def cmd_my_profile(message: types.Message):
+    user = await UserDB(message.from_user.id).connect()
+    account = await user.tokens.get_current_acc()
+    token = await user.tokens.get_current_token()
+    pages = await telegraph_requests.get_pages_count(token)
+    notes = await user.notes.get_notes_count()
+    groups = await user.notes.get_groups_count()
+    text = (
+        f'<b>Текущий аккаунт:</b> {account}\n'+
+        f'<b>Статей аккаунта:</b> {pages}\n'+
+        f'<b>Заметок:</b> {notes}\n'
+        f'<b>Групп:</b> {groups}\n'
+    )
+    await message.answer(text)
+
+
+def register_my_profile(dp: Dispatcher):
+    dp.register_message_handler(cmd_my_profile, commands='profile')
