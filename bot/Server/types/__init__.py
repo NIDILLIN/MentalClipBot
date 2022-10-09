189d0be267bd4e07af9df56c6cf3dab99ffb5540
NIDILLIN <writebutread@outlook.com>
2022-10-09 23:05:35 +0300

    Note to save to db

--- bot/Server/types/__init__.py
+++ bot/Server/types/__init__.py
@@ -1,12 +1,33 @@
+from typing import Optional
 
 
-class Note():
-    tag: str
-    text: str
+class Note(dict):
+    tag: Optional[str]=None
+    text: Optional[str]=None
+    photo: Optional[str]=None
+    document: Optional[str]=None
+    tagged_text: Optional[str]=None
 
-    def __init__(self, tag: str, text: str) -> None:
-        self.tag = tag
-        self.text = text
+
+    def __init__(
+        self, 
+        tag: Optional[str]=None,
+        text: Optional[str]=None, 
+        photo: Optional[str]=None, 
+        document: Optional[str]=None
+    ) -> None:
+        self['tag'] = tag
+        self['text'] = text
+        self['photo'] = photo
+        self['document'] = document
+        self.tag_text()
+
+    def tag_text(self) -> None:
+        self['tagged_text'] = (
+            f"{self['tag']}"+
+            '\n\n'+
+            f"{self['text']}"
+        )
 
 
 class Article():
