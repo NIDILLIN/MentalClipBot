459b1952cbdd5174a7de2d62e5a60e262012988a
NIDILLIN <writebutread@outlook.com>
2022-10-09 23:05:16 +0300

    React only to startState users

--- bot/Server/handlers/fast_note/fast_note.py
+++ bot/Server/handlers/fast_note/fast_note.py
@@ -1,10 +1,9 @@
 from aiogram import Dispatcher
 from aiogram import types
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
+from Server.handlers.commands.common import startState
 
-from Server.handlers.commands.new_note import add_note, add_tag
+from Server.handlers.commands.notes.new_note import add_note
 
 
 def register_fast_note(dp: Dispatcher) -> None:
-    dp.register_message_handler(add_note, content_types=types.ContentTypes.TEXT)
+    dp.register_message_handler(add_note, content_types=types.ContentTypes.TEXT, state=startState)
